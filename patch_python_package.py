0b45a979d225cc4f1680e032f2bd63aa7ee4d56a
Vinyzu <50874994+Vinyzu@users.noreply.github.com>
2025-09-07 13:00:27 +0200

    Set Version for Fix Update

--- patch_python_package.py
+++ patch_python_package.py
@@ -5,7 +5,7 @@ import os
 import toml
 
 patchright_version = os.environ.get('playwright_version')
-# patchright_version = "1.55.0"
+patchright_version = "1.55.1"
 
 def patch_file(file_path: str, patched_tree: ast.AST) -> None:
     with open(file_path, "w") as f:
